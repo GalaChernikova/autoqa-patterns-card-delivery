ae864c386ff453909f67c27866e16974176767c2
Gala Chernikova <gala.chernikova94@gmail.com>
2022-10-07 14:08:51 +0300

    Update DataGenerator

--- src/test/java/ru/netology/delivery/data/DataGenerator.java
+++ src/test/java/ru/netology/delivery/data/DataGenerator.java
@@ -16,24 +16,31 @@ public class DataGenerator {
     public static String generateDate(int shift) {
         // TODO: добавить логику для объявления переменной date и задания её значения, для генерации строки с датой
         // Вы можете использовать класс LocalDate и его методы для получения и форматирования даты
+        String date = LocalDate.now().plusDays(shift).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
         return date;
     }
 
     public static String generateCity(String locale) {
         // TODO: добавить логику для объявления переменной city и задания её значения, генерацию можно выполнить
         // с помощью Faker, либо используя массив валидных городов и класс Random
+        Faker faker = new Faker(new Locale(locale));
+        String city = generateCity(faker.address().city());
         return city;
     }
 
     public static String generateName(String locale) {
         // TODO: добавить логику для объявления переменной name и задания её значения, для генерации можно
         // использовать Faker
+        Faker faker = new Faker(new Locale(locale));
+        String name = generateName(faker.name().firstName() + "" + faker.name().lastName());
         return name;
     }
 
     public static String generatePhone(String locale) {
         // TODO: добавить логику для объявления переменной phone и задания её значения, для генерации можно
         // использовать Faker
+        Faker faker = new Faker(new Locale(locale));
+        String phone = generatePhone(faker.phoneNumber().phoneNumber());
         return phone;
     }
 
@@ -44,6 +51,8 @@ public class DataGenerator {
         public static UserInfo generateUser(String locale) {
             // TODO: добавить логику для создания пользователя user с использованием методов generateCity(locale),
             // generateName(locale), generatePhone(locale)
+            Faker faker = new Faker(new Locale(locale));
+            var user = new UserInfo(generateCity(locale), generateName(locale), generatePhone(locale));
             return user;
         }
     }
